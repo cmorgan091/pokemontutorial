b7412ce6d1881ac529b819023341ae26a9d884db
Chris Morgan <chris@morganits.com>
2021-06-06 00:33:27 +0100

    Setup pokemon with moves (end of #6)

--- Assets/Scripts/Pokemon/Move.cs
+++ Assets/Scripts/Pokemon/Move.cs
@@ -7,10 +7,10 @@ public class Move
     public MoveBase Base { get; set; }
     public int PP { get; set; }
 
-    public Move(MoveBase pBase, int pp)
+    public Move(MoveBase pBase)
     {
         Base = pBase;
-        PP = pp;
+        PP = pBase.PP;
     }
 
 }
--- Assets/Scripts/Pokemon/Pokemon.cs
+++ Assets/Scripts/Pokemon/Pokemon.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class Pokemon
@@ -7,10 +8,21 @@ public class Pokemon
     PokemonBase _base;
     int level;
 
+    public int HP { get; set; }
+
+    public List<Move> Moves { get; set; }
+
     public Pokemon(PokemonBase pBase, int pLevel)
     {
         _base = pBase;
         level = pLevel;
+        HP = _base.MaxHp;
+
+        Moves = _base.LearnableMoves
+            .Where(x => x.Level <= level)
+            .Select(x => new Move(x.MoveBase))
+            .Take(4)
+            .ToList();
     }
 
     public int Attack => Mathf.FloorToInt((_base.Attack * level) / 100f) + 5;
